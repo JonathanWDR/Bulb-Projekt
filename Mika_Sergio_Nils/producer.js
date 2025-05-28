9104f2ba67c9b2b1a5f48eaf39a264fb25da19f9
i589076 <mika.jun@sap.com>
2025-05-28 17:24:18 +0200

    initial commit

---
+++ Mika_Sergio_Nils/producer.js
@@ -0,0 +1,28 @@
+import amqp from 'amqplib';
+
+async function produceLampCommands() {
+  const queueName = 'lamp-commands';
+
+  try {
+    const connection = await amqp.connect('amqp://localhost');
+    const channel = await connection.createChannel();
+
+    await channel.assertQueue(queueName, { durable: false });
+
+    const command = { command: 'color', value: 'red' };
+
+    const msgBuffer = Buffer.from(JSON.stringify(command));
+    channel.sendToQueue(queueName, msgBuffer);
+    console.log('[x] Sent:', command);
+
+    setTimeout(async () => {
+      await channel.close();
+      await connection.close();
+    }, 500);
+
+  } catch (error) {
+    console.error('Error in producer:', error);
+  }
+}
+
+produceLampCommands();
