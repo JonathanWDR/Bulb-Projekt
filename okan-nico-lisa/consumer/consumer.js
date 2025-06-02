82f8b6a3903e199bb640fa3272346f12de9756b8
Nico Wrede <nico.wrede@sap.com>
2025-06-02 15:00:16 +0200

    Move backend folder, add initial test calls to TPLink API, add .gitignore

--- okan-nico-lisa/consumer/consumer.js
+++ okan-nico-lisa/consumer/consumer.js
@@ -1,7 +1,9 @@
 const amqp = require('amqplib');
-// const { Client } = require('tplink-bulb');
+import * as TPLink from 'tplink-bulbs';
+require('dotenv').config()
 
 async function startConsumer() {
+
   const conn = await amqp.connect('amqp://rabbitmq');
   const channel = await conn.createChannel();
   const exchange = 'lamp_control';
@@ -12,6 +14,38 @@ async function startConsumer() {
   // Required actions for bulb
   ['on','off','brightness','color','morse'].forEach(key => channel.bindQueue(q.queue, exchange, key));
 
+  // TP Link Controls
+  const email = process.env.TP_Email;
+  const password = process.env.TP_Passwd;
+  const deviceId = process.env.TP_Device_Id;
+
+  const cloudApi = await TPLink.API.cloudLogin(email, password);
+    
+  const devices = await cloudApi.listDevicesByType('SMART.TAPOBULB');
+  console.log(devices);
+  const targetDevice = devices.find(device => device.deviceId === deviceId);
+
+  if (!targetDevice) {
+    console.error(`Device with ID "${deviceId}" not found.`);
+    return;
+  }
+
+  const device = await TPLink.API.loginDevice(email, password, targetDevice);
+
+  const info = await device.getDeviceInfo();
+  console.log('Device Info:', info);
+
+  await device.turnOn();
+  await device.setColour('violet');
+  await TPLink.API.delay(500);
+
+  await device.setColour('red');
+  await TPLink.API.delay(500);
+
+  await device.setColour('orange');
+  await TPLink.API.delay(500);
+
+  // Consume events...
   console.log('Consumer wartet auf Events...');
   channel.consume(q.queue, msg => {
     if (!msg) return;
@@ -25,6 +59,11 @@ async function startConsumer() {
     switch(action) {
       case "on":
         console.log('Lampe einschalten...')
+        device.turnOn();
+        break
+      case "off":
+        console.log('Lampe ausschalten...')
+        device.turnOff();
         break
       // ... ToDo
     }
