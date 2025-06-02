af4bb9388cec9da627d880f0f4a11ad29cd030bb
Okan Sönmez <okso2004@gmail.com>
2025-06-03 01:11:00 +0200

    add missing logic to consumer (color doesnt work)

--- okan-nico-lisa/consumer/consumer.mjs
+++ okan-nico-lisa/consumer/consumer.mjs
@@ -1,6 +1,7 @@
 import * as TPLink from 'tplink-bulbs';
 import * as amqp from 'amqplib'
 import * as dotenv from 'dotenv'
+import { flashMorse } from './morse.js';
 
 async function startConsumer() {
   dotenv.config();
@@ -22,9 +23,8 @@ async function startConsumer() {
   const deviceIpAddress = process.env.TP_Device_Ip;
 
   const cloudApi = await TPLink.API.cloudLogin(email, password);
-    
   const devices = await cloudApi.listDevicesByType('SMART.TAPOBULB');
-  console.log(devices);
+  console.log('Devices: ', devices);
   const targetDevice = devices.find(device => device.deviceId === deviceId);
 
   if (!targetDevice) {
@@ -32,34 +32,63 @@ async function startConsumer() {
     return;
   }
 
-  console.log('Logging in...')
+  console.log('Logging in to TPLink device...')
   const device = await TPLink.API.loginDeviceByIp(email, password, deviceIpAddress)
-  console.log('Logged in!')
+  console.log('Logged in on bulb named', device.nickname)
 
   const info = await device.getDeviceInfo();
   console.log('Device info:', info);
 
   // Consume events...
   console.log('Consumer waiting for events...');
-  channel.consume(q.queue, msg => {
+  channel.consume(q.queue, async (msg) => {
     if (!msg) return;
-    // Message received
+    // Message received through channel
     const action = msg.fields.routingKey;
     const payload = JSON.parse(msg.content.toString());
 
     console.log(`In: ${action} ->`, payload);
 
-    //TODO: Bulb controls
-    switch(action) {
-      case "on":
-        console.log('Turning on bulb...')
-        device.turnOn();
-        break
-      case "off":
-        console.log('Turning off bulb...')
-        device.turnOff();
-        break
-      // ... ToDo
+    try {
+      switch(action) {
+        case "on":
+          console.log('Turning on bulb...')
+          await device.turnOn();
+          break
+        case "off":
+          console.log('Turning off bulb...')
+          await device.turnOff();
+          break
+        case "brightness":
+          const brightnessString = payload['value'];
+          const brightness = parseInt(brightnessString)
+          if (typeof brightness === 'number' && brightness > 0 && brightness <= 100) {
+            console.log(`Setting brightness: ${brightness}%...`);
+            await device.setBrightness(brightness);
+          } else {
+            console.log('Invalid brightness:', brightness);
+          }
+          break;
+        case "color":
+          const color = payload['value']
+          console.log(`Changing color to ${color}...`)
+          // ToDo: potentially not working properly in lib?
+          await device.setColour(color)
+          break;
+        case "morse":
+          const morse = payload['value']
+          if (typeof morse === 'string' && morse.length > 0) {
+            console.log(`Morsing text "${morse}"...`);
+            await flashMorse(device, morse);
+          } else {
+            console.log('Invalid morse code:', payload);
+          }
+          break;
+        default:
+          console.log('Received unknown action type:', action);
+      }
+    } catch (err) {
+      console.log('Error on handling event: ' + err)
     }
 
     channel.ack(msg);
