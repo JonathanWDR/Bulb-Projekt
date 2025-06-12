61d152f04e996f95b952ea6a2875b7fe55edcff4
Jochern <johannes.kling@outlook.de>
2025-06-12 14:28:50 +0200

    Update Dockerfile and entrypoint script: enhance package installation and increase ping attempts for ARP setup

--- Christian_Ibrahim_Jochen/backend/consumer/entrypoint.sh
+++ Christian_Ibrahim_Jochen/backend/consumer/entrypoint.sh
@@ -6,7 +6,7 @@ TARGET_IP=192.168.216.238
 # Einmal ARP erzwingen – auch wenn keine Antwort kommt
 echo "running setup…"
 echo "Führe initialen Ping zu $TARGET_IP aus, um ARP zu triggern..."
-ping -c 1 -W 1 $TARGET_IP > /dev/null || true
+ping -c 5  $TARGET_IP > /dev/null || true
 
 # Jetzt 2 Sekunden warten, damit ARP-Cache sich füllt
 sleep 2
